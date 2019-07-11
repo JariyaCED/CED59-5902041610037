f639726f69f34effbd51ed92cb30da75571fefce
Jariya Palagawong <jariya.paga@gmail.com>
2019-07-11 14:39:23 +0700

    add files for assignment 3

---
+++ assignment3/func.py
@@ -0,0 +1,5 @@
+def return_():
+	return 42;
+
+def my_method():
+	return n1 * n2 ; 
---
+++ assignment3/main.py
@@ -0,0 +1,4 @@
+from func import *
+print("result of return_42() is",return_42());
+
+print("result of 5 x 6 is ",my_method(5,6));
