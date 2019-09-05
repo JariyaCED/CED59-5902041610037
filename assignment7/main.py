2a954ca9984aa384f09a61fc5ce050f387fb3360
JariyaCED <jariya.paga@gmail.com>
2019-09-05 16:33:47 +0700

    addNEW

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -6,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return "Hi"
-    return render_template('register.html')
+    return render_template('re.html')
 
 @app.route('/',methods=["post"])
 def register():
