1ebee6c31e74b0c9bc9903eca96935a952dd7f5f
JariyaCED <jariya.paga@gmail.com>
2019-09-19 16:15:14 +0700

    LN

---
+++ assignment8/common/models/Subject.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "subject".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $deteil
+ */
+class Subject extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'subject';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['deteil'], 'string'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'deteil' => 'Deteil',
+        ];
+    }
+}
